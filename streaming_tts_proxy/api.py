1d16a4240805288c66c69a03bd38d5b1da3659b1
mitrokun <108380896+mitrokun@users.noreply.github.com>
2025-06-12 10:48:25 +0300

    Update api.py

--- streaming_tts_proxy/api.py
+++ streaming_tts_proxy/api.py
@@ -11,7 +11,6 @@ from wyoming.info import Describe, Info
 
 _LOGGER = logging.getLogger(__name__)
 
-# Определяем наши кастомные ошибки здесь, в одном месте
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
 
